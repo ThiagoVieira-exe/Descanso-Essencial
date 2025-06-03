98dd009d82378f966ee6714c8d86b296e47d4c91
Thiago Vieira <devthiagovieira@gmail.com>
2025-06-03 14:29:10 -0300

    Add botao voltar ao topo

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,27 @@
-window.addEventListener("scroll", () => {
-  const links = document.querySelectorAll("nav a");
+window.addEventListener('scroll', () => {
+  const links = document.querySelectorAll('nav a');
   const fromTop = window.scrollY + 100;
 
-  links.forEach((link) => {
+  links.forEach(link => {
     const section = document.querySelector(link.hash);
     if (
       section.offsetTop <= fromTop &&
       section.offsetTop + section.offsetHeight > fromTop
     ) {
-      link.classList.add("active");
+      link.classList.add('active');
     } else {
-      link.classList.remove("active");
+      link.classList.remove('active');
     }
   });
 
-  const backToTopBtn = document.getElementById("backToTop");
+  const btn = document.getElementById('btnTop');
   if (window.scrollY > 400) {
-    backToTopBtn.style.display = "flex";
+    btn.style.display = 'block';
   } else {
-    backToTopBtn.style.display = "none";
+    btn.style.display = 'none';
   }
 });
 
 function scrollToTop() {
-  window.scrollTo({ top: 0, behavior: "smooth" });
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 }
